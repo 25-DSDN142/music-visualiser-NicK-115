916c2039af45c9e4a0780d018ea74a4e68f2435b
NicK-115 <kspisawesome17@gmail.com>
2025-09-09 12:28:31 +1200

    First stage of Outside + Improving Explosion FX
    Also added minor adjustments to other aspects, mainly the visualizer bars. Updated README.md, too.

--- music_starter.js
+++ music_starter.js
@@ -18,7 +18,7 @@ let introVisualizerTimes = [816,842,876, 900,930,960, 984,1020,1050, 1074,1104,1
 let introVisualizerTypes = [];
 
 let CoreVisualizerX;
-let CoreVisualizerTimes = [4308,4320,4348,4368,4380,4408,4434,4452];
+let CoreVisualizerTimes = [4308,4320,4348,4368,4390,4408,4434,4452];
 let CoreVisualizerTypes = [];
 
 
@@ -33,7 +33,7 @@ let visualBarsFlash2BlueCore;
 let visualBarsFlash2BlueCoreTransparent;
 
 let vignette_fadeout_elapsed = 0;
-let sirenrotation = 0;
+let sirenRotation = 0;
 
 let warningIntroAlpha = 0;
 let warningHoriOffset = 0;
@@ -50,10 +50,37 @@ let bar_fadeout_elapsed = 0;
 let coreSize;
 let coreSize2;
 let coreSizeBase = 0;
+let coreY = 0;
+let coreYStep = 0;
 
 let chargeAlpha = 255;
 let chargeSizeBase = 200;
 let x;
+
+let twinkleSize = 0;
+let twinkleRotation = 0;
+
+let balconyY = 0;
+let bridgeY = 0;
+
+function twinkle(x,y, spikeSizeX=40,spikeSizeY=40, centerThicknessX=10,centerThicknessY=10) {
+
+  beginShape();
+  vertex(x-centerThicknessX,y+centerThicknessY); //Spike Left
+  vertex(x-spikeSizeX,y);
+
+  vertex(x-centerThicknessX,y-centerThicknessY); //Spike Up
+  vertex(x,y-spikeSizeY);
+
+  vertex(x+centerThicknessX,y-centerThicknessY); //Spike Right
+  vertex(x+spikeSizeX,y);
+
+  vertex(x+centerThicknessX,y+centerThicknessY); //Spike Down
+  vertex(x,y+spikeSizeY);
+
+  endShape(CLOSE);
+
+}
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
@@ -92,16 +119,25 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       warningHoriOffset = 0;
       warningRiseOffset = 0;
       vignette_fadeout_elapsed = 0;
-      sirenrotation = 0;
+      sirenRotation = 0;
 
       bar_fadeout_elapsed = 0;
 
 
       coreSizeBase = 0;
+      coreY = 0;
+      coreYStep = 0;
 
       chargeAlpha = 255;
       chargeSizeBase = 350;
       x = 0;
+
+      twinkleSize = 0;
+      twinkleRotation = 0;
+
+      balconyY = height;
+      bridgeY = height/3;
+
    }
 
 // changes 
@@ -127,44 +163,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       if (counter > 120 && introHeightBase > 0){
          introHeightBase-=8;
       }
-   }
-
-   //Red Vignette Effects
-   if (counter == 3258 || counter == 3270) {
-      vignette_fadeout_elapsed = 0;
-   }
-   if (counter > 3258 && counter < 3270) {
-      fill(160-(vignette_fadeout_elapsed),20,20);
-      rect(0,0, width,height);
-      fill(20,32);
-      for (let i = 0; i < 12; i++){
-         ellipse(0,0, (width*0.9)-(width*i*0.025),(height*0.9)-(height*i*0.025))
-      }
-      vignette_fadeout_elapsed += 3.5;
-   } else if (counter > 3270 && vignette_fadeout_elapsed < 140) {
-      fill(160-(vignette_fadeout_elapsed),20,20);
-      rect(0,0, width,height);
-      fill(20,32);
-      for (let i = 0; i < 12; i++){
-         ellipse(0,0, (width*0.9)-(width*i*0.025),(height*0.9)-(height*i*0.025))
-      }
-      vignette_fadeout_elapsed += 3.5;
-   }
-
-   //Siren Effect
-   if (counter > 1860 && counter < 4313){
-      fill(255,0,0,128);
-      translate(0,-height/1.5);
-      rotate(sirenrotation);
-      beginShape();
-      vertex(width*2,height/2);
-      vertex(-width*2,-height/2);
-      vertex(-width*2,height/2);
-      vertex(width*2,-height/2);
-      endShape(CLOSE);
-      rotate(-sirenrotation);
-      translate(0,height/1.5);
-      sirenrotation+=2;
    }
 
    //Intro Flood Effects 
@@ -200,28 +198,62 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
          floodBarsSub1X-=(width/100);
       }
    }
-   //Main
-   if (counter > 120 && counter < 5000) {
-      floodBarsBass = map(bass, 0,100, floodHeight,floodHeight*1.25)
-      floodBarsX = -width/2-((counter*3) % width*.72);
-      if (counter > 1860){
-         warningColorRange = map(drum, 0,100, 0,1);
-         warningColor = lerpColor(warningCyan,warningWhite, warningColorRange);
-         fill(warningColor);
-      } else {
-        fill(100,200,255);
+
+   //Siren Effect
+   if (counter > 1860 && counter < 4313){
+      if (counter < 3258 || counter > 3276){
+         fill(255,0,0,128);
+         translate(0,-height/1.5);
+         rotate(sirenRotation);
+         beginShape();
+         vertex(width*2,height/2);
+         vertex(-width*2,-height/2);
+         vertex(-width*2,height/2);
+         vertex(width*2,-height/2);
+         endShape(CLOSE);
+         rotate(-sirenRotation);
+         translate(0,height/1.5);
       }
-      if (floodHeight < 420 && counter < 1512) {
-         floodHeight+=0.5;
-      } else if (floodHeight > 160 && counter < 1860) {
-         floodHeight-=0.5;
-      } else if (counter > 4308) {
-         floodBarsY+=0.5;
+      sirenRotation+=2;
+   }
+
+ //Box to hide BG elements behind Bars Temporarily During RisingBlasts
+   if (counter > 3096 && counter < 3258) {
+      barsHide = map(counter, 3096,3258, 0,1);
+      barsHideColor = lerpColor(color(20,0),color(20,255),barsHide);
+      fill(barsHideColor);
+      rect(0,0, width, height);
+   }
+
+   //Red Vignette Effects
+   
+   if (counter == 3258 || counter == 3270) {
+      vignette_fadeout_elapsed = 0;
+   }
+   if (counter > 3258 && counter < 3271) {
+      fill(160-(vignette_fadeout_elapsed),20,20);
+      rect(0,0, width,height);
+      fill(20,32);
+      for (let i = 0; i < 12; i++){
+         ellipse(0,0, (width*0.9)-(width*i*0.025),(height*0.9)-(height*i*0.025))
       }
-      for (let i = 0; i < 360; i++){
-         rect(floodBarsX,floodBarsY, (width/100)+4,floodBarsBass+(sin(i*5)*height/25));
-         floodBarsX+=(width/100);
+      vignette_fadeout_elapsed += 3.5;
+   } else if (counter > 3270 && counter < 3276) {
+      fill(160-(vignette_fadeout_elapsed),20,20);
+      rect(0,0, width,height);
+      fill(20,32);
+      for (let i = 0; i < 12; i++){
+         ellipse(0,0, (width*0.9)-(width*i*0.025),(height*0.9)-(height*i*0.025))
       }
+      vignette_fadeout_elapsed += 23;
+   }
+
+ //Box to hide BG elements behind Bars Temporarily During RisingBlasts
+   if (counter > 3276 && counter < 3306) {
+      FlashMap = map(counter, 3276,3306, 0.75,1);
+      FlashColor = lerpColor(color(255,255),color(255,0),FlashMap);
+      fill(FlashColor);
+      rect(0,0, width, height);
    }
 
    //VisualBars
@@ -258,7 +290,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
             if (counter > 4470 && counter < 4751) {
                fill(visualBarsFlash2BlueCoreTransparent);
-               rect(introVisualizerX,0, (width/96)+visualBarsBlueCoreSizeElapsed*width/250,(height/5)*(prepFadeout/100)+introBarValue+visualBarsBlueCoreSizeElapsed*width/250);
+               rect(introVisualizerX,0, (width/96)+visualBarsBlueCoreSizeElapsed*width/250,(height/5)*(0.05)+introBarValue*(introBarValueMulti/100)+visualBarsBlueCoreSizeElapsed*width/250);
             }
             fill(visualBarsFlash2BlueCore);
          } else if (counter > 1686){
@@ -382,12 +414,36 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
    }
 
-   //Box to hide Visualizer Bars Temporarily
+   //Box to hide Visualizer Bars Temporarily for Explosion
    if (counter > 6144) {
       barsHide = map(counter, 6144,6312, 0,1);
       barsHideColor = lerpColor(color(20,0),color(20,255),barsHide);
       fill(barsHideColor);
       rect(0,0, width, height);
+   }
+
+   //MainFlood (Moved Here for Layering Reasons)
+   if (counter > 120 && counter < 5000) {
+      floodBarsBass = map(bass, 0,100, floodHeight,floodHeight*1.25)
+      floodBarsX = -width/2-((counter*3) % width*.72);
+      if (counter > 1860){
+         warningColorRange = map(drum, 0,100, 0,1);
+         warningColor = lerpColor(warningCyan,warningWhite, warningColorRange);
+         fill(warningColor);
+      } else {
+        fill(100,200,255);
+      }
+      if (floodHeight < 420 && counter < 1512) {
+         floodHeight+=0.5;
+      } else if (floodHeight > 160 && counter < 1860) {
+         floodHeight-=0.5;
+      } else if (counter > 4308) {
+         floodBarsY+=0.5;
+      }
+      for (let i = 0; i < 360; i++){
+         rect(floodBarsX,floodBarsY, (width/100)+4,floodBarsBass+(sin(i*5)*height/25));
+         floodBarsX+=(width/100);
+      }
    }
 
    //Rising Blasts
@@ -460,14 +516,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       }
    }
 
-   //Box to hide Everything Temporarily During RisingBlasts
-   if (counter > 3096 && counter < 3258) {
-      barsHide = map(counter, 3096,3276, 0,.75);
-      barsHideColor = lerpColor(color(20,0),color(20,255),barsHide);
-      fill(barsHideColor);
-      rect(0,0, width, height);
-   }
-
    //The Core
 
    //Charge
@@ -484,11 +532,41 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       circle(0,0, chargeSizeBase+(x/1.5));
    }
 
+   //Pre Explosion Twinkle FX
+   if (counter > 6270 && counter < 6330) {
+
+      twinkleGrow = map(counter, 6270,6300, 0,height/8);
+      twinkleShrink = map(counter, 6300,6330, height/8,0);
+
+      fill(100,200,255);
+
+      if (counter < 6300) {twinkleSize = twinkleGrow;} else {twinkleSize = twinkleShrink;}
+      rotate(twinkleRotation);
+      twinkle(0,0,twinkleSize,twinkleSize,twinkleSize/4,twinkleSize/4);
+      rotate(-twinkleRotation);
+      twinkleRotation+=4;
+
+   }
+
+   //ExplosionPulseFX
+   if (counter > 6330) {
+      for (let i = 0; i < 20; i++) {
+         BlastFXTime = map(counter, 6330+(i*5),6360+(i*5), 0,1);
+         BlastFXColor = lerpColor(color(100,200,255,128), color(100,200,255,0), BlastFXTime);
+         if (counter > 6330+(i*10)) {
+            fill(BlastFXColor);
+            circle(0,0, coreSizeBase+BlastFXTime*height);
+         }
+      }
+   }
+
+   //Post Explosion City Background
+
    //Blast Bars
    if (counter > 6330) {
-      BarVocal = map(vocal, 0,100, coreSizeBase*1.25,coreSizeBase*1.75);
       BarDrum = map(drum, 0,100, coreSizeBase*1.25,coreSizeBase*1.75);
       BarBass = map(bass, 0,100, coreSizeBase*1.25,coreSizeBase*1.75);
+      BarOther = map(other, 0,100, coreSizeBase*1.25,coreSizeBase*1.75);
       if (counter > 6330 && counter < 7410 && bar_fadeout_elapsed < 100) {
       bar_fadeout_elapsed+=10;
       } else if (counter > 7410 && bar_fadeout_elapsed > 1) {
@@ -496,7 +574,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       }
       for (let i = 0; i < 120; i++) {
          fill(113,225,255);
-         rect(0,0, 5,BarVocal*(bar_fadeout_elapsed/100));
+         rect(0,0, 5,BarOther*(bar_fadeout_elapsed/100));
          rotate(3);
          rect(0,0, 5,BarDrum*(bar_fadeout_elapsed/100));
          rotate(3);                                                                                                 
@@ -518,6 +596,8 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
          coreSizeBase = 150;
       } else if (counter > 6330 && counter < 7410) {
          coreSizeBase+=0.5;
+      } else if (counter > 8862) {
+         coreSizeBase+=1.5;
       }
 
       coreSize = map(vocal, 0,100, coreSizeBase*1,coreSizeBase*1.25);
@@ -527,5 +607,59 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
       coreSize2 = map(vocal, 0,100, coreSizeBase*1.1,coreSizeBase*1.35);
       fill(100,200,255,128);
       circle(0,0, coreSize2);
+   }
+
+   //Post Explosion
+   //Box to semi-obscure Core after the Explosion
+   if (counter > 7380) {
+      CoreMask = map(counter, 7380,7470, 0,1);
+      CoreMaskColor = lerpColor(color(20,0),color(20,64),CoreMask);
+      CoreMask2 = map(counter, 8778,8862, 1,0);
+      CoreMaskColor2 = lerpColor(color(20,0),color(20,64),CoreMask2);
+      if (counter > 8777) {fill(CoreMaskColor2);} else if (counter < 7470) {fill(CoreMaskColor);} else {fill(20,64)}
+      rect(0,0, width, height);
+   }   
+   //Foreground
+   //Bridge
+   if (counter > 7380) {
+
+      if (counter > 8184) {
+         bridgeY = map(counter, 8184,8244, height/10,height/3);
+         bridgeColorMap = map(counter, 8184,8244, 1,0);
+         bridgeColor = lerpColor(color(128,0),color(128,255),bridgeColorMap);
+      } else if (counter > 7470) {
+         bridgeY = height/10;
+         bridgeColor = color(128);
+      } else {
+         bridgeY = map(counter, 7380,7470, height/3,height/10);
+         bridgeColorMap = map(counter, 7380,7470, 0,1);
+         bridgeColor = lerpColor(color(128,0),color(128,255),bridgeColorMap);
       }
+
+      fill(bridgeColor);
+      rect(0,bridgeY, width,(height/20));
+      rect(0,bridgeY+height/2+height/40, width/50,height);
+      rect(width/5,bridgeY+height/2+height/40, width/50,height);
+      rect(-width/5,bridgeY+height/2+height/40, width/50,height);
+      rect(width/2.5,bridgeY+height/2+height/40, width/50,height);
+      rect(-width/2.5,bridgeY+height/2+height/40, width/50,height);
+   }
+   //Balcony
+   if (counter > 7380) {
+      if (counter > 7470) {balconyY = height/2} else {balconyY = map(counter, 7380,7470, height,height/2);}
+      fill(100,200,255,64);
+      rect(0,balconyY, width,(height/1.5));
+      fill(64);
+      rect(0,balconyY, width,(height/3));
+      rect(0,balconyY-height/3, width,(height/48));
+      fill(0,50,0);
+      rect(0,balconyY, width,(height/4));
+   }
+
+
+
+
+
+
+
 }
